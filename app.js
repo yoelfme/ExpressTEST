9540b8c5e1a1c6b2189877928eb74cb95a3ac9bc
Yoel Monzón <yoelfme@hotmail.com>
2015-02-02 00:03:32 -0600

    Test routes, and install dependencies

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var http = require('http');
-var app = express();
+var express = require('express'),
+	http = require('http'),
+	app = express(),
+	bodyParser = require('body-parser');
 
+// Middleware
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	extended: true
+}));
+
+
+// Set port
 app.set('port',process.env.PORT || 3000);
 
-http.createServer(app).listen(app.get('port'),function(){
+http.createServer(app).listen(app.get('port'),function() {
 	console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.get('/',function(req,res) {
+	res.send('Hola Express');
+});
+
+// With parameters by GET
+app.get('/users/:username',function(req, res) {
+	var username = req.params.username;
+	res.send('Hola ' + username);
+});
+
+// With parameters by POST
+app.post('/users',function(req, res) {
+	var username = req.body.username;
+	res.send('Hola ' + username);
 });
