cb52ad30d9a768e59ae45c75db2321b39a36a2c4
Yoel Monzón <yoelfme@hotmail.com>
2015-02-04 11:31:14 -0600

    Config package.json, and test Jade

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser');
 
+// Set port
+app.set('port',process.env.PORT || 3000);
+
+// Set motor of views
+app.set('views', __dirname + '/views');
+app.set('view engine','jade');
+
 // Middleware
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 	extended: true
 }));
 
-
-// Set port
-app.set('port',process.env.PORT || 3000);
-
-http.createServer(app).listen(app.get('port'),function() {
-	console.log('Express server listening on port ' + app.get('port'));
-});
-
 app.get('/',function(req,res) {
-	res.send('Hola Express');
+	// res.send('Hola Express');
+	res.render('index', {
+		title_page: 'CHAT',
+		title: 'Bienvenidos',
+		username: 'Bienvenidos'
+	});
 });
 
 // With parameters by GET
@@ -31,4 +35,21 @@ app.get('/users/:username',function(req, res) {
 app.post('/users',function(req, res) {
 	var username = req.body.username;
 	res.send('Hola ' + username);
+});
+
+// With regular expresions
+app.get(/\/personal\/(\d*)\/?(edit)?/,function(req, res) {
+	var message = 'el perfil del usuario #' + req.params[0];
+
+	if (req.params[1]=== 'edit') {
+		message = 'Editando ' + message;
+	}else {
+		message = 'Viendo ' + message;
+	};
+
+	res.send(message);
+});
+
+http.createServer(app).listen(app.get('port'),function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
